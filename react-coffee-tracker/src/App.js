04511dfdd6dcccceda3be8e9f04fc4c7e4a7f475
Stephanie <stephanie@fonk.amsterdam>
2022-04-28 20:39:07 +0200

    save

--- react-coffee-tracker/src/App.js
+++ react-coffee-tracker/src/App.js
@@ -20,11 +20,11 @@ function App() {
 
       <Routes>
         {/* {CoffeeRoutes} */}
-        <Route path="/react-coffee-project/coffees" exact element={<CoffeesPage />} />
-        <Route path="/react-coffee-project/coffees/:id" element={<CoffeeDetailPage />} />
-        <Route path="/react-coffee-project/roasteries" exact element={<RoasteriesPage />} />
-        <Route path="/react-coffee-project/methods" exact element={<MethodsPage />} />
-        <Route path="/react-coffee-project/" element={<CoffeesPage />} />
+        <Route path="/coffees" exact element={<CoffeesPage />} />
+        <Route path="/coffees/:id" element={<CoffeeDetailPage />} />
+        <Route path="/roasteries" exact element={<RoasteriesPage />} />
+        <Route path="/methods" exact element={<MethodsPage />} />
+        <Route path="/" element={<CoffeesPage />} />
       </Routes>
     </div >
   );
